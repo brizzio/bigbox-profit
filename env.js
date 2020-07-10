e4014adf2b1f69012fbb07dd96ddefb8441f69dd
brizzio <fabrizio.salvade@gmail.com>
2020-07-10 14:59:30 -0300

    Update env.js

--- env.js
+++ env.js
@@ -1,12 +1,11 @@
 module.exports = {
-    //DATABASE_URL:'postgres://postgres:pr0f1t#2020@3.219.57.146:5432/giga',
-    PORT:5253,
-    SERVER_PORT:5566,
-    DB_GIGA:{
-        user: 'postgres',
-        host: '3.219.57.146',
-        database: 'postgres',
-        password: 'pr0f1t#2020',
-        port: 5432
+    PORT:9000,
+    SERVER_PORT:9000,
+    DB_CREDENTIALS:{
+        user: 'db_user',
+        host: 'ip_addres',
+        database: 'database name',
+        password: 'user_password',
+        port: 'database port'
     }
 }
