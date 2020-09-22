cd4013eb60da536c457a12de6163a20162efdb30
brizzio <fabrizio.salvade@gmail.com>
2020-09-22 18:55:09 -0300

    alterações no controller

--- app/controllers/gestaoPrecosController.js
+++ app/controllers/gestaoPrecosController.js
@@ -18,11 +18,10 @@ const {
    * @returns {object} array de categorias
    */
   const getAllFilters = async (req, res) => {
-    const strQuery = `SELECT bandeiras, clusters, lojas, departamentos, secoes, categorias, sub_categorias, fornecedores, papel_categoria, sensibilidade, produtos
-    FROM bigbox.mat_view_filtros;`;
+    const strQuery = `select * from pricing_bigbox.mv_filters;`;
     try {
       const { rows } = await dbQuery.query(strQuery);
-      console.log(JSON.stringify(rows))
+     // console.log(JSON.stringify(rows))
       const dbResponse = rows;
       if (dbResponse[0] === undefined) {
         errorMessage.error = 'Não Existem Filtros';
@@ -35,6 +34,11 @@ const {
       return res.status(status.error).send(errorMessage);
     }
   };
+
+
+
+
+
 
 
   /**
@@ -52,7 +56,7 @@ const {
     const strQuery = `select array(select t.descricao from bigbox.lojas t where t.cluster = (select c.cod_cluster from bigbox.clusters c where descricao ='${req.params.cluster}'));`
     try {
       const { rows } = await dbQuery.query(strQuery);
-      console.log(JSON.stringify(rows))
+      //console.log(JSON.stringify(rows))
       const dbResponse = rows;
       if (dbResponse[0] === undefined) {
         errorMessage.error = 'Não Existem Lojas';
@@ -77,10 +81,10 @@ const {
     
     try {
       const pages = await dbQuery.query(`select count(*) from bigbox.gestao_precos_view;`);
-      console.log(JSON.stringify(pages.rows[0].count))
+      //console.log(JSON.stringify(pages.rows[0].count))
       var registros = pages.rows[0].count
       const { rows } = await dbQuery.query(strQuery);
-      console.log(JSON.stringify(rows))
+      //console.log(JSON.stringify(rows))
 
       const dbResponse = rows;
       if (dbResponse[0] === undefined) {
@@ -102,14 +106,14 @@ const {
 
   const getTabelaGestaoPrecos = async (req, res) => {
     
-    const strQuery = `select * from bigbox.gestao_precos_view;`
+    const strQuery = `select * from pricing_bigbox.vw_gestao_preco_calculada where cluster_simulador = 'BIG BOX';`
     
     try {
-      const pages = await dbQuery.query(`select count(*) from bigbox.gestao_precos_view;`);
-      console.log(JSON.stringify(pages.rows[0].count))
-      var registros = pages.rows[0].count
+      const pages = await dbQuery.query(`select count(*) from pricing_bigbox.vw_gestao_preco_calculada where cluster_simulador = 'BIG BOX';`);
+      //console.log(JSON.stringify(pages.rows[0].count))
+      var registros = pages.rows[0]
       const { rows } = await dbQuery.query(strQuery);
-      console.log(JSON.stringify(rows))
+      //console.log(JSON.stringify(rows))
 
       const dbResponse = rows;
       if (dbResponse[0] === undefined) {
@@ -124,6 +128,31 @@ const {
      
       return res.status(status.success).send(successMessage);
     } catch (error) {
+      errorMessage.getTAbelaGestaoPrecos_error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
+
+  const getGestaoTotalizadores = async (req, res) => {
+    
+    const strQuery = `select * from pricing_bigbox.filtro_multiplo_totalizador(${req.body.grupo},${req.body.fornecedor},${req.body.produto},${req.body.bandeira},${req.body.sensibilidade},${req.body.papel_categoria},${req.body.sub_grupo},${req.body.cluster},${req.body.departamento},${req.body.secao})`
+    
+    try {
+      
+      const { rows } = await dbQuery.query(strQuery);
+      //console.log(JSON.stringify(rows))
+
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = 'Não Existem Totais';
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.data = dbResponse;
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
       errorMessage.error = JSON.stringify(error);
       return res.status(status.error).send(errorMessage);
     }
@@ -131,8 +160,182 @@ const {
 
 
 
+
+
+  const getGeoLojas = async (req, res) => {
+    
+    const strQuery = `select * from bigbox.cadastro_lojas;`
+    
+    try {
+      
+      const { rows } = await dbQuery.query(strQuery);
+      //console.log(JSON.stringify(rows))
+
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = 'Não Existem Lojas';
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.data = dbResponse;
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
+  const getGeoConcorrentes = async (req, res) => {
+    
+    const strQuery = `select * from pricing_bigbox.cadastro_concorrentes;`
+    
+    try {
+      
+      const { rows } = await dbQuery.query(strQuery);
+      //console.log(JSON.stringify(rows))
+
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = 'Não Existem Lojas';
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.data = dbResponse;
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
+  const filterTable = async (req, res) => {
+    
+    const filters = req.body
+    console.log(req.body)
+   
+   const strQuery = `
+      select * from pricing_bigbox.filtro_multiplo_pais_proporcionais(${req.body.grupo},${req.body.fornecedor},${req.body.produto},${req.body.bandeira},${req.body.sensibilidade},${req.body.papel_categoria},${req.body.sub_grupo},${req.body.cluster},${req.body.departamento},${req.body.secao})`
+    try {
+      
+      const { rows } = await dbQuery.query(strQuery);
+      //console.log(JSON.stringify(rows))
+
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = JSON.stringify(req.body);
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.data = dbResponse;
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
+  const filterTablePaisFilhos = async (req, res) => {
+    
+    const filters = req.body
+    console.log(req.body)
+   
+   const strQuery = `
+      select * from pricing_bigbox.filtro_multiplo_gestao_preco_paifilho (${req.body.grupo},${req.body.fornecedor},${req.body.produto},${req.body.bandeira},${req.body.sensibilidade},${req.body.papel_categoria},${req.body.sub_grupo},${req.body.cluster},${req.body.departamento},${req.body.secao})`
+    try {
+      
+      const { rows } = await dbQuery.query(strQuery);
+      console.log(rows)
+
+      const dbResponse = rows;
+      //console.log('COUNT : ' + dbResponse.lenght)
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = JSON.stringify(req.body);
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.registros = dbResponse.length;
+      successMessage.data = dbResponse;
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
+  const filtroDependente = async (req, res) => {
+    
+    const filters = req.body
+    console.log(req.body)
+
+   //parametros de entrada
+   //departamento,secao,grupo,subgrupo,produto,fornecedor
+   
+   const strQuery = `
+      select * from pricing_bigbox.filtro_dependente2(${req.body.departamento},${req.body.secao},${req.body.grupo},${req.body.sub_grupo},${req.body.produto},${req.body.fornecedor})`
+    try {
+      
+      const { rows } = await dbQuery.query(strQuery);
+      //console.log(JSON.stringify(rows))
+
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = 'Não Existem filtros';
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.data = dbResponse;
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
+  const getFilhosByPaiProporcional = async (req, res) => {
+    var pagina, page_items
+    
+    var cluster = req.params.cluster
+    var cod_pai_proporcional = req.params.codigo_pai_proporcional
+    //pagina=req.params.pagina 
+    //var offs = (pagina -1)*page_items
+    const strQuery = `select * from pricing_bigbox.tratar_dados_pais_filhos_proporcionais
+    where cluster_simulador = '${cluster}' and cod_pai_proporcao ='${cod_pai_proporcional}';`
+    
+    try {
+       const { rows } = await dbQuery.query(strQuery);
+      //console.log(JSON.stringify(rows))
+
+      const dbResponse = rows;
+      if (dbResponse[0] === undefined) {
+        errorMessage.error = 'Não Existem Filhos ou Proporcionais';
+        return res.status(status.notfound).send(errorMessage);
+      }
+      
+      successMessage.registros = dbResponse.length;
+      successMessage.data = dbResponse;
+      
+     
+      return res.status(status.success).send(successMessage);
+    } catch (error) {
+      errorMessage.error = JSON.stringify(error);
+      return res.status(status.error).send(errorMessage);
+    }
+  };
+
   module.exports = {getAllFilters, 
                     getLojasNoCluster,
                     getDadosGestaoPrecos,
-                    getTabelaGestaoPrecos
+                    getTabelaGestaoPrecos,
+                    getGeoLojas,
+                    getGeoConcorrentes,
+                    getGestaoTotalizadores,
+                    filterTable,
+                    filterTablePaisFilhos,
+                    filtroDependente,
+                    getFilhosByPaiProporcional
                     }
--- app/routes/gestaoPrecosRoute.js
+++ app/routes/gestaoPrecosRoute.js
@@ -3,7 +3,14 @@ var express = require('express');
 var { getAllFilters,
       getLojasNoCluster,
       getDadosGestaoPrecos,
-      getTabelaGestaoPrecos } = require('../controllers/gestaoPrecosController');
+      getTabelaGestaoPrecos,
+      getGeoLojas,
+      getGeoConcorrentes,
+      getGestaoTotalizadores,
+      filterTable,
+      filterTablePaisFilhos,
+      filtroDependente,
+      getFilhosByPaiProporcional} = require('../controllers/gestaoPrecosController');
 //var verifyAuth = require('../middlewares/verifyAuth');
 
 const router = express.Router();
@@ -19,10 +26,27 @@ router.get('/lojas/:cluster', getLojasNoCluster);
 //Retorna os dados da gestão de precos com paginacao
 router.get('/gestao/tabela', getTabelaGestaoPrecos);
 
+//Retorna os totais da gestão de precos com paginacao
+router.post('/gestao/totalizadores', getGestaoTotalizadores);
+
 //Retorna os dados da gestão de precos com paginacao
 router.get('/gestao/:itensPorPagina/:pagina', getDadosGestaoPrecos);
 
+//rotas para dados de geolocalização e referencia geografica /api/v1/geo/lojas
+router.get('/geo/lojas', getGeoLojas);
+router.get('/geo/concorrentes', getGeoConcorrentes);
 
+//filtra a tabela gestão de precos e retorna registros filtrados
+router.post('/gestao/filtro', filterTable);
+
+//filtra a tabela gestão de precos e retorna todos os registros (pais e filhos) dos itens filtrados
+router.post('/gestao/filtro/paisefilhos', filterTablePaisFilhos);
+
+//filtra a tabela gestão de precos e retorna todos os registros (pais e filhos) associados a um pai proporcional
+router.get('/gestao/filtro/paisefilhos/:cluster/:codigo_pai_proporcional', getFilhosByPaiProporcional);
+
+
+//filtra a tabela gestão de precos e retorna registros filtrados
+router.post('/gestao/filtro-dependentes', filtroDependente);
 
 module.exports = router;
-
